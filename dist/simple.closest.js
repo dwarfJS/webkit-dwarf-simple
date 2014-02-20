80ca556c80381719c7fb82aa01d8f97beb144908
Daniel Yang <miniflycn@justany.net>
2014-02-20 10:42:09 +0800

    update simple.closest

---
+++ dist/simple.closest.js
@@ -0,0 +1,42 @@
+/*!
+ * webkit-dwarf-simple-closest
+ * @author donaldyang
+ */
+
+define('./simple.closest', ['./simple'], function (require, exports, module) {
+    'use strict';
+
+    var $ = require('./simple');
+
+    var de = document.documentElement, 
+        match = de.webkitMatchesSelector || de.matchesSelector;
+
+    function _matchElement(selector, refElement) {
+        refElement = refElement || de;
+        return match.call(refElement, selector);
+    }
+
+    /**
+     * closest
+     * @param {String} selector
+     * @param {DOM} context
+     */
+    function closest(selector, context) {
+        var res = null;
+        context = $(context);
+        context.every(function (cur) {
+            while (cur.nodeType === 1) {
+                if (_matchElement(selector, cur)) {
+                    res = cur;
+                    return false;
+                } else {
+                    cur = cur.parentNode;
+                }
+            }
+            return true;
+        });
+        return res;
+    }
+
+    return module.exports = closest;
+});
